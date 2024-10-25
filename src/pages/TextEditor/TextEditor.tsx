802df5452fd1bbf0733f1af007729e49b908fc0a
nara <parknara0213@gmail.com>
2024-10-26 01:05:36 +0900

    Feat: 커서 위치 지정
    Refs: #1

--- src/pages/TextEditor/TextEditor.tsx
+++ src/pages/TextEditor/TextEditor.tsx
@@ -1,12 +1,36 @@
-import te from "./TextEditor.module.css";
+import { setCursorPosition } from "../../utils/text-editor";
+import styles from "./TextEditor.module.css";
 
 const TextEditor = () => {
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = e.target;
+    console.log("클릭한 요소", target);
+
+    const className = (target as HTMLElement).className;
+
+    // 클릭한 요소가 줄인 경우
+    if (className.includes("line")) {
+      // 줄의 마지막 요소의 마지막
+      const lastChild = (target as HTMLElement).lastElementChild
+        ?.children[0] as HTMLElement;
+      if (!lastChild) return;
+      console.log("마지막 요소", lastChild);
+      const text = lastChild?.textContent || "";
+
+      setCursorPosition(lastChild, text.length);
+    }
+  };
+
   return (
     <div className="text-editor">
-      <div className={te.content}>
-        <div className={te.line}>
-          <span>
-            <span className={te.normal} contentEditable></span>
+      <div className={styles.content} onClick={(e) => handleClick(e)}>
+        <div className={styles.line}>
+          {/* 선택 영역 지정을 위해서 span을 이중으로 설계해야 함 */}
+          <span className={`${styles.container}`}>
+            <span className={styles.span} contentEditable></span>
+          </span>
+          <span className={`${styles.container}`}>
+            <span className={styles.span} contentEditable></span>
           </span>
         </div>
       </div>

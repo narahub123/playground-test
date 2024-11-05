23cbb49aefd9e597b06a58fe7247bf39dafcb9e2
nara <parknara0213@gmail.com>
2024-11-05 10:51:18 +0900

    Feat: 추가 비디어 업로드하기
    Refs: #24

--- src/pages/Video/Upload.tsx
+++ src/pages/Video/Upload.tsx
@@ -1,7 +1,33 @@
 import styles from "./Upload.module.css";
+import { FileType } from "./VideoContainer";
 
-const Upload = () => {
-  return <div className="upload">Upload</div>;
+interface UploadProps {
+  setFile: React.Dispatch<React.SetStateAction<FileType>>;
+}
+
+const Upload = ({ setFile }: UploadProps) => {
+  const uploadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    console.log("파일 정보", file);
+
+    // 미리 보기
+    const url = URL.createObjectURL(file);
+    console.log("createObjectURL에 의해 생성된 URL 정보", url);
+
+    // 비디오 추가
+    if (file.type.includes("video")) {
+      setFile({
+        url,
+        video: file.type.includes("video"),
+      });
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <input type="file" onChange={(e) => uploadFile(e)} />
+    </div>
+  );
 };
 
 export default Upload;
--- src/pages/Video/VideoContainer.tsx
+++ src/pages/Video/VideoContainer.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import Upload from "./Upload";
 import styles from "./VideoContainer.module.css";
+export interface FileType {
+  url: string;
+  video: boolean;
+}
 
 const VideoContainer = () => {
-  return <div className={styles.wrapper}>VideoContainer</div>;
+  const [file, setFile] = useState<FileType>({
+    url: "",
+    video: false,
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <Upload setFile={setFile} />
+      {file.video && <video src={file.url} controls width={"250px"} />}
+    </div>
+  );
 };
 
 export default VideoContainer;
+

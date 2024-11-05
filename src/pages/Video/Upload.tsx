02375ed75bdb62f2a85fdaad554156d8c18c7fc2
nara <parknara0213@gmail.com>
2024-11-05 11:20:13 +0900

    Feat: 추가 비디오 용량 제한
    Refs: #25

--- src/pages/Video/Upload.tsx
+++ src/pages/Video/Upload.tsx
@@ -11,6 +11,18 @@ const Upload = ({ setFile }: UploadProps) => {
     if (!file) return;
     console.log("파일 정보", file);
 
+    const fileSize = file.size;
+    console.log("파일 사이즈", fileSize);
+
+    // 파일 용량 제한하기
+    const maxSize = 10 * 1024 * 1024; // 10mb
+    if (fileSize > maxSize) {
+      window.alert("10mb 이하의 동영상을 사용해주세요.");
+
+      setFile({ url: "", video: false });
+      return;
+    }
+
     // 미리 보기
     const url = URL.createObjectURL(file);
     console.log("createObjectURL에 의해 생성된 URL 정보", url);

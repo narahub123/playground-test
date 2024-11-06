8f8a8aa850e9b1489b5d6b3d52660de2f1cfab36
nara <parknara0213@gmail.com>
2024-11-06 17:07:43 +0900

    Feat: 추가 드래그를 통한 재생 시간 이동
    Refs: #28

--- src/pages/Video/Playbar.tsx
+++ src/pages/Video/Playbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Playbar.module.css";
 import { TimeType } from "./Video";
 type PlaybarProps = {
@@ -10,6 +10,7 @@ const Playbar = ({ time, setTime, videoRef }: PlaybarProps) => {
   const { curTime, duration } = time;
   const timeRef = useRef<HTMLDivElement>(null);
   const trackRef = useRef<HTMLDivElement>(null);
+  const [isClicked, setIsClicked] = useState(false);
 
   // 시간 흐름에 따른 플레이 바 변화
   useEffect(() => {
@@ -25,6 +26,58 @@ const Playbar = ({ time, setTime, videoRef }: PlaybarProps) => {
   // 클릭을 통한 시간 이동
   const handleclick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (!trackRef.current || !timeRef.current || !videoRef.current) return;
+
+    const track = trackRef.current;
+    const playbar = timeRef.current;
+    const video = videoRef.current;
+
+    const curXPosition = e.clientX;
+    console.log("클릭한 위치", curXPosition);
+
+    // thumb 초기 위치
+    const startPosition = track.getBoundingClientRect().left;
+    console.log("초기 위치", startPosition);
+
+    // track 길이
+    const trackWidth = track.getBoundingClientRect().width;
+    console.log("트랙 길이", trackWidth);
+
+    // 이동 거리
+    const distance = curXPosition - startPosition;
+
+    console.log("이동거리", distance);
+
+    // 이동거리 / 트랙 길이
+    const percent = distance / trackWidth;
+    console.log("퍼센트", percent * 100);
+
+    // thumb 이동 시키기
+    playbar.style.width = percent * 100 + "%";
+
+    // 재생 시간 이동하기
+    console.log(duration * percent);
+    const newTime = duration * percent;
+    video.currentTime = newTime;
+    setTime((prev) => ({ ...prev, curTime: newTime }));
+  };
+
+  // 드래그를 통한 시간 이동
+  // thumb을 클릭하는 경우
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsClicked(true);
+  };
+  const handleMouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsClicked(false);
+  };
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (
+      !isClicked ||
+      !trackRef.current ||
+      !timeRef.current ||
+      !videoRef.current
+    )
+      return;
+
     const track = trackRef.current;
     const playbar = timeRef.current;
     const video = videoRef.current;
@@ -60,9 +113,14 @@ const Playbar = ({ time, setTime, videoRef }: PlaybarProps) => {
   };
 
   console.log("현재 시간", time.curTime);
+  console.log(isClicked);
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onMouseMove={isClicked ? (e) => handleMouseMove(e) : undefined}
+      onMouseUp={(e) => handleMouseUp(e)}
+    >
       <div className={styles.trackWrapper}>
         <div
           className={styles.track}
@@ -71,7 +129,10 @@ const Playbar = ({ time, setTime, videoRef }: PlaybarProps) => {
         >
           <div className={styles.time} ref={timeRef}>
             <div className={styles.thumbWrapper}>
-              <div className={styles.thumb} />
+              <div
+                className={styles.thumb}
+                onMouseDown={(e) => handleMouseDown(e)}
+              />
             </div>
           </div>
         </div>

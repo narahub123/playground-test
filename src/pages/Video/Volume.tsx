ef3a5e2c6ee1e50b54a4502564f8d33d6c64a390
nara <parknara0213@gmail.com>
2024-11-06 10:37:39 +0900

    Feat: 추가 드래그를 통한 음량 조절
    Refs: #27

--- src/pages/Video/Volume.tsx
+++ src/pages/Video/Volume.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styles from "./Volume.module.css";
 
 interface VolumeProps {
@@ -10,18 +10,22 @@ interface VolumeProps {
 const Volume = ({ videoRef, setVolume, setIsMuted }: VolumeProps) => {
   const trackRef = useRef<HTMLDivElement>(null);
   const volumeRef = useRef<HTMLDivElement>(null);
+  const bottom = Math.floor(
+    trackRef.current?.getBoundingClientRect().bottom || 0
+  );
+  // console.log("트랙 바닥 위치", bottom);
 
+  const [isClicked, setIsClicked] = useState(false);
+
+  // 클릭을 통한 음량 조절
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (!trackRef.current || !volumeRef.current || !videoRef.current) return;
 
     const volume = volumeRef.current;
     const video = videoRef.current;
 
-    const bottom = Math.floor(trackRef.current.getBoundingClientRect().bottom);
-    console.log("트랙 바닥 위치", bottom);
-
     const curYPosition = e.clientY;
-    console.log("현재의 y 위치", curYPosition);
+    // console.log("현재의 y 위치", curYPosition);
 
     let height = bottom - curYPosition;
 
@@ -36,16 +40,64 @@ const Volume = ({ videoRef, setVolume, setIsMuted }: VolumeProps) => {
       setIsMuted(false);
     }
 
-    console.log("높이", height);
-    console.log("음량", height / 100);
+    // console.log("높이", height);
+    // console.log("음량", height / 100);
 
     video.volume = height / 100;
     setVolume(height / 100);
     volume.style.height = height + "px";
   };
 
+  // thumb을 눌렀을 때
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log("mouse down");
+    setIsClicked(true);
+  };
+  // thumb에서 마우스를 뺐을 때
+  const handleMouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log("mouse up");
+    setIsClicked(false);
+  };
+
+  // thumb이 클릭된 상태에서 마우스가 움직인 경우
+  const hanldeMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    // thumb이 클릭된 상태가 아니라면 리턴
+    if (!isClicked || !videoRef.current || !volumeRef.current) return;
+    const video = videoRef.current;
+    const height = volumeRef.current;
+
+    // 현재 Y 위치
+    const curYPosition = e.clientY;
+    console.log("현재 Y 위치", curYPosition);
+
+    let sound = bottom - curYPosition;
+    console.log("음량", sound);
+
+    if (sound <= 0) {
+      sound = 0;
+      setIsMuted(true);
+    } else if (0 < sound && sound <= 100) {
+      sound = bottom - curYPosition;
+      setIsMuted(false);
+    } else if (100 < sound) {
+      sound = 100;
+      setIsMuted(false);
+    }
+
+    // 실제 음량 조절
+    video.volume = sound / 100;
+    // 음량 상태 업데이트
+    setVolume(sound / 100);
+    // 음량 높이 변경
+    height.style.height = sound + "px";
+  };
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={styles.wrapper}
+      onMouseMove={isClicked ? (e) => hanldeMouseMove(e) : undefined}
+      onMouseUp={(e) => handleMouseUp(e)}
+    >
       <div className={styles.container}>
         <div className={styles.cover}>
           <div
@@ -55,7 +107,10 @@ const Volume = ({ videoRef, setVolume, setIsMuted }: VolumeProps) => {
           >
             <div className={styles.volume} ref={volumeRef}>
               <div className={styles.thumbWrapper}>
-                <div className={styles.thumb} />
+                <div
+                  className={styles.thumb}
+                  onMouseDown={(e) => handleMouseDown(e)}
+                />
               </div>
             </div>
           </div>

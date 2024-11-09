8d2f4855ac87a2b8055f0ce3f21a6afd2ce57ae6
nara <parknara0213@gmail.com>
2024-11-09 20:46:05 +0900

    저장

--- src/pages/Notification/NotificationContainer.tsx
+++ src/pages/Notification/NotificationContainer.tsx
@@ -46,9 +46,9 @@ const NotificationContainer = () => {
     }
   }, []);
 
-  const text = "아! 아리먼";
+  //   const text = "아! 아리먼";
 
-  const notification = new Notification("zz", { body: text, icon: icon });
+  //   const notification = new Notification("zz", { body: text, icon: icon });
 
   return <div className={styles.container}></div>;
 };
